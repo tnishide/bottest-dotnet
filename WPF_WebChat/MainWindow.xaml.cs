c63ddfc3dedf12007bff70aa0d5497d8d781a1e1
sawa <sawa@vision.kuee.kyoto-u.ac.jp>
2017-08-02 17:50:37 +0900

    URL変更

--- WPF_WebChat/MainWindow.xaml.cs
+++ WPF_WebChat/MainWindow.xaml.cs
@@ -1,33 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_WebChat
 {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
     {
         public MainWindow()
         {
 
             InitializeComponent();
+			//var uri = @"file:///C:/researchS/15MicrosoftBotFramework/BotFramework-WebChat/samples/fullwindow/index.html?domain=http://localhost:3000/directline&webSocket=false&username=WPF-User";
+			var uri = @"file:///C:/Users/sawa/Source/Repos/BotFramework-WebChat-Test/samples/backchannel/index.html?domain=http://localhost:3000/directline&webSocket=false&pi=100";
 
-            wbWebChat.Navigate(new Uri(@"file:///C:/researchS/15MicrosoftBotFramework/BotFramework-WebChat/samples/fullwindow/index.html?domain=http://localhost:3000/directline&webSocket=false&username=WPF-User"));
+            wbWebChat.Navigate(new Uri(uri));
             // レスポンスを早くするには index.html で pollingInterval をpiで指定できるようにしたうえで，Navigate内で &pi=100 を追加する
 
         }
